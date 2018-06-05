894da83ac7c6f35efdd084ac6fcc1323f9fd52b0
Jacob Gelman <jacobgelman@icloud.com>
2018-06-05 12:11:52 -0700

    Fix `isTimed` option

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ class GPIOValveAccessory {
         this.firmwareRevision = config.firmwareRevision || packageFile.version
 
         // Timer & duration
-        this.isTimed = config.isTimed || true
+        this.isTimed = !!config.isTimed
 
         if (this.isTimed) {
             this.defaultDuration  = config.defaultDuration  || 600
